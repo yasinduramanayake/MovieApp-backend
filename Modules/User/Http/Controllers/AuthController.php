a6470c56315bb34cd9a40b75fcae8d35caad8bf1
yasinduramanayake <yasinduramanayake123@gmail.com>
2021-07-16 20:05:23 +0530

    'updated'

---
+++ Modules/User/Http/Controllers/AuthController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Modules\User\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Contracts\Routing\ResponseFactory;
+use Illuminate\Http\Response;
+use Auth;
+use Modules\User\Entities\User;
+use Modules\User\Http\Requests\UserRegisterRequest;
+use Modules\User\Http\Requests\UserSignInRequest;
+use Illuminate\Validation\ValidationException;
+use Modules\User\Http\Resources\UserResource;
+
+class AuthController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware(["auth:api-system-user"])->except([
+            "login",
+            "register",
+            "profile",
+        ]);
+    }
+
+    /**
+     * @param UserRegisterRequest $request
+     * @return ResponseFactory|Response
+     */
+
+    public function register(UserRegisterRequest $request)
+    {
+        $data = $request->validated();
+        $data["password"] = bcrypt($data["password"]);
+
+        $user = User::create($data);
+
+        return response([
+            "data" => $user,
+            "token" => $user->createToken("api-system-user")->accessToken,
+        ]);
+    }
+
+    /**
+     * @param UserSignInRequest $request
+     * @return ResponseFactory|Response
+     * @throws ValidationException
+     */
+    public function login(UserSignInRequest $request)
+    {
+        if (!Auth::attempt($request->validated())) {
+            throw ValidationException::withMessages([
+                "login" => "Invalid Credentials",
+            ]);
+        }
+
+        return response([
+            "data" => auth()->user(),
+            "token" => auth()
+                ->user()
+                ->createToken("api-system-user")->accessToken,
+        ]);
+    }
+    /**
+     * Show user profile.
+     *
+     * @return UserResource
+     */
+
+    public function profile()
+    {
+        return new UserResource(auth()->user());
+    }
+}
---
+++ Modules/User/Routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Modules\User\Http\Controllers\AuthController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware("auth:api")->get("/users", function (Request $request) {
+    return $request->user();
+});
+
+Route::post("register", [AuthController::class, "register"]);
+Route::post("login", [AuthController::class, "login"]);
+Route::get("profile", [AuthController::class, "profile"]);
