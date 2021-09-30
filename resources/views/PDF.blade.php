225a15230e0291bd64fa6639f1ad948b177347fd
yasinduramanayake <yasinduramanayake123@gmail.com>
2021-09-30 17:18:39 +0530

    updated

--- resources/views/PDF.blade.php
+++ resources/views/PDF.blade.php
@@ -10,12 +10,14 @@
 
     <table border="1">
         <thead>
+            <th>Theater Image</th>
             <th>Theater Name</th>
             <th>Theater Type</th>
             <th>Theater Venue</th>
             <th>Time 1</th>
             <th>Time 2</th>
-            <th>Movies</th>
+            <th>Total Theaters</th>
+
 
         </thead>
         <tbody>
@@ -23,11 +25,14 @@
             </tr>
             @foreach($data as $theaters)
             <tr>
+                <td><img src="{{$theaters->image}}"></td>
                 <td>{{$theaters->name}}</td>
                 <td>{{$theaters->type}}</td>
                 <td>{{$theaters->venue}}</td>
                 <td>{{$theaters->time1}}</td>
                 <td>{{$theaters->time2}}</td>
+                <td>{{count($data)}}</td>
+
             </tr>
             @endforeach
         </tbody>
